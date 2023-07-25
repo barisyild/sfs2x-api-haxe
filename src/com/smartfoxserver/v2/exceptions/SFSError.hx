ef18b8c7ec6f686cf5c6cd0ef79a88cbb65ea810
barisyild <fb_bcy@hotmail.com>
2023-07-25 18:45:52 +0300

    OpenFL 9.2.0 compile fix

--- src/com/smartfoxserver/v2/exceptions/SFSError.hx
+++ src/com/smartfoxserver/v2/exceptions/SFSError.hx
@@ -6,16 +6,23 @@ import com.smartfoxserver.v2.errors.Error;
 class SFSError extends Error
 {
 	private var _details:String;
-	
+
 	public function new(message:String, errorId:Int=0, extra:String=null)
 	{
 		super(message, errorId);
 		_details=extra;
 	}
-	
+
+	#if (openfl <= "9.1.0")
 	public var details(get, null):String;
- 	private function get_details():String
+ 	private override function get_details():String
 	{
 		return _details;
 	}
+	#else
+	public function details():String
+	{
+		return _details;
+	}
+	#end
 }
